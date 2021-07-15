a1e0edead81f5f4b04a1bb4dd5c4afce5211a0d7
Stefano Manfredi <56640837+stemanfredi@users.noreply.github.com>
2021-07-15 11:17:44 +0200

    Database initialization

--- src/script.js
+++ src/script.js
@@ -1,10 +1,10 @@
 'use strict'
 
 document.addEventListener('DOMContentLoaded', () => {
-  
   const wall = document.querySelector('.wall')
   const loginForm = document.querySelector('#login')
   const nav = document.querySelector('nav')
+  const navBtn = document.querySelectorAll('.nav__btn')
   const views = document.querySelectorAll('.view')
 
   // Login form behaviour
@@ -23,12 +23,20 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   })
 
-  // Navigation links behaviour
-  nav.addEventListener('click', e => {
-    if (e.target.matches('[data-link]')) {
+  // Navigation buttons behaviour
+  navBtn.forEach(item => {
+    item.addEventListener('click', e => {
       e.preventDefault()
       // Get selected view from clicked link
-      const selectedView = e.target.getAttribute('data-link')     
+      const selectedView = e.target.getAttribute('data-link')
+      // Change buttons appearance
+      navBtn.forEach(item => {
+        if (item.getAttribute('data-link') === selectedView) {
+          item.classList.add('nav__btn--selected')
+        } else {
+          item.classList.remove('nav__btn--selected')
+        }
+      })
       // Show only selected view
       views.forEach(item => {
         if (item.id === selectedView) {
@@ -37,8 +45,90 @@ document.addEventListener('DOMContentLoaded', () => {
           item.classList.add('hidden')
         }
       })
+    })
+  })
+
+  // DATABASE OPERATION
+  let db
+
+  const serv = document.querySelector('#serv')
+  const nSeriale = document.querySelector('#nSeriale')
+  const tipo = document.querySelector('#tipo')
+  const nProgr = document.querySelector('#nProgr')
+  const prior = document.querySelector('#prior')
+  const from = document.querySelector('#from')
+  const to = document.querySelector('#to')
+  const info = document.querySelector('#info')
+  const nomeEC = document.querySelector('#nomeEC')
+
+  const reportForm = document.querySelector('#reportForm')
+
+  let request = window.indexedDB.open('restore', 1)
+
+  request.onerror = function () {
+    console.log('Database failed to open')
+  }
+
+  request.onsuccess = function () {
+    console.log('Database opened successfully')
+    db = request.result
+  }
+
+  request.onupgradeneeded = function (e) {
+    let db = e.target.result
+    let objectStore = db.createObjectStore('reports', {
+      keyPath: 'id',
+      autoIncrement: true,
+    })
+
+    //objectStore.createIndex('id', 'id', { unique: false })
+
+    console.log('Database setup complete')
+  }
+
+  function addData(e) {
+    e.preventDefault()
+    let newItem = {
+      id: serv.value + nSeriale.value + tipo.value + nProgr.value,
+      serv: serv.value,
+      nSeriale: nSeriale.value,
+      tipo: tipo.value,
+      nProgr: nProgr.value,
+      prior: prior.value,
+      from: from.value,
+      to: to.value,
+      info: info.value,
+      nomeEC: nomeEC.value,
     }
-  })  
+
+    console.log(newItem)
+
+    let transaction = db.transaction(['reports'], 'readwrite')
+    let objectStore = transaction.objectStore('reports')
+    let request = objectStore.add(newItem)
+
+    request.onsuccess = () => {
+      //serv.value = ''
+      //nSeriale.value++
+      //tipo.value = ''
+      //nProgr.value = ''
+      //prior.value = ''
+      //from.value = ''
+      //to.value = ''
+      //info.value = ''
+      //nomeEC.value = ''
+    }
+
+    transaction.oncomplete = () => {
+      console.log('Transaction completed on the database')
+    }
+
+    transaction.onerror = () => {
+      console.log('Transaction not completed, error!')
+    }
+  }
+
+  reportForm.onsubmit = addData
 })
 
 // Ask for confirmation to exit ro reload the page
